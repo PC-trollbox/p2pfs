aaa7bc846d1a0b9dd5e89f330236ca74d2e83732
PC-trollbox <71093117+PC-trollbox@users.noreply.github.com>
2023-07-14 14:18:46 +0300

    Add Node.JS scripts

---
+++ nodeScripts/nodedown.js
@@ -0,0 +1,74 @@
+const io = require("socket.io-client");
+const fs = require("fs");
+const util = require("util");
+const params = util.parseArgs({
+    options: {
+        server: {
+            type: "string",
+            short: "s",
+            default: "https://p2pfs-repl.pcprojects.tk/"
+        },
+        "show-download-stats": {
+            type: "boolean",
+            short: "d",
+            default: false
+        },
+        "help": {
+            type: "boolean",
+            short: "h",
+            default: false
+        },
+        "downloadid": {
+            type: "string",
+            short: "i"
+        }
+    },
+    allowPositionals: true
+});
+const avg = (arr) => arr.reduce((a, b) => a + b, 0) / arr.length;
+
+if (params.values.help) {
+    console.log("nodedown ... [endfile]");
+    console.log("\t--server, -s <server-url> : server url");
+    console.log("\t--show-download-stats, -d : show download stats");
+    console.log("\t--help, -h : show the help page");
+    console.log("\t--downloadid, -i <download-id> : download ID in the form of socket ID, IP or alias");
+    console.log("\t[endfile] : file that will be the result of the download");
+} else {
+    let socket = io(params.values.server);
+    socket.on("connect", function() {
+        socket.emit("download_request", params.values.downloadid);
+        socket.once("download_ack", function(handshakeData) {
+            let endFile = params.positionals[0] ? fs.createWriteStream(params.positionals[0]) : process.stdout;
+            let speeds = [];
+            let transferredBytesPerSecond = 0;
+            let previousSpeedDate = Date.now();
+            socket.once("chunk", function chunkHandler(chunkData, ack) {
+                endFile.write(chunkData.chunk);
+                if (params.values["show-download-stats"]) {
+                    transferredBytesPerSecond += handshakeData.chunkSize;
+                    if (previousSpeedDate + 1000 < Date.now()) {
+                        previousSpeedDate = Date.now();
+                        speeds.push(transferredBytesPerSecond);
+                        transferredBytesPerSecond = 0;
+                    }
+                    process.stderr.write("\rdownloaded " + bytesToReadable(Math.min(chunkData.offset + handshakeData.chunkSize, handshakeData.fileSize)) + " out of " + bytesToReadable(handshakeData.fileSize) + " | " + bytesToReadable(avg(speeds)) + "/s");
+                }
+                ack();
+                socket.once("chunk", chunkHandler);
+                if ((chunkData.offset + handshakeData.chunkSize) >= handshakeData.fileSize) process.exit(0);
+            });
+        });
+    });
+}
+
+function bytesToReadable(bytes) {
+    bytes = Math.floor(bytes);
+    let readable = "";
+    if (Math.floor(bytes / 1024 / 1024 / 1024)) readable = readable + Math.floor(bytes / 1024 / 1024 / 1024) + " GB ";
+    if (Math.floor(bytes / 1024 / 1024 % 1024)) readable = readable + Math.floor(bytes / 1024 / 1024 % 1024) + " MB ";
+    if (Math.floor(bytes / 1024 % 1024)) readable = readable + Math.floor(bytes / 1024 % 1024) + " KB ";
+    if (Math.floor(bytes % 1024)) readable = readable + (bytes % 1024) + " B ";
+    readable = readable.trim();
+    return readable;
+}
---
+++ nodeScripts/nodeup.js
@@ -0,0 +1,94 @@
+const io = require("socket.io-client");
+const fs = require("fs");
+const util = require("util");
+const params = util.parseArgs({
+    options: {
+        server: {
+            type: "string",
+            short: "s",
+            default: "https://p2pfs-repl.pcprojects.tk/"
+        },
+        "help": {
+            type: "boolean",
+            short: "h",
+            default: false
+        },
+        "downloadid": {
+            type: "string",
+            short: "i"
+        },
+        "show-download-id": {
+            type: "string",
+            short: "d",
+            multiple: true
+        },
+        "stop-after-download": {
+            type: "boolean",
+            short: "o"
+        },
+        "chunk": {
+            type: "string",
+            short: "c",
+            default: "1048576"
+        }
+    },
+    allowPositionals: true
+});
+const avg = (arr) => arr.reduce((a, b) => a + b, 0) / arr.length;
+
+if (params.values.help) {
+    console.log("nodeup ... [infile]");
+    console.log("\t--server, -s <server-url> : server url");
+    console.log("\t--help, -h : show the help page");
+    console.log("\t--downloadid, -i <download-id> : download alias");
+    console.log("\t--show-download-id, -d : show download id. can be socket, ip, alias.");
+    console.log("\t--stop-after-download, -o : stop running after one download");
+    console.log("\t--chunk, -c : set chunk size, bytes");
+    console.log("\t[infile] : file that will be the result of the upload");
+} else {
+    let socket = io(params.values.server);
+    socket.on("connect", function() {
+        let correctOptions = params.values["show-download-id"].filter(a => ["socket", "ip", "alias"].includes(a));
+        socket.emit("connAlias", params.values.downloadid);
+        socket.once("useIP", (b) => correctOptions = correctOptions.map(a => a == "ip" ? b : a));
+        socket.once("connAliasValid", () => correctOptions = correctOptions.map(a => a == "alias" ? params.values.downloadid : a));
+        correctOptions = correctOptions.map(a => a == "socket" ? socket.id : a);
+        if (correctOptions.includes("ip") || correctOptions.includes("alias")) setTimeout(() => console.log(correctOptions.join(",")), 1000);
+        else console.log(correctOptions.join(","));
+        socket.once("download", async function downloadHandler(handshakeData) {
+            let file = params.positionals[0];
+            let fileStream = fs.openSync(file, "r");
+            let fileSize = fs.statSync(file).size;
+            socket.emit("download_ack", {
+                id: handshakeData.id,
+                fileSize: fileSize,
+                chunkSize: Number(params.values.chunk)
+            });
+            if (!params.values["stop-after-download"]) socket.once("download", downloadHandler);
+            if (params.values["stop-after-download"]) process.exit(0);
+
+            let offset = 0;
+            let buf = Buffer.alloc(Math.min(fileSize - offset, Number(params.values.chunk)));
+            fs.readSync(fileStream, buf, 0, Math.min(fileSize - offset, Number(params.values.chunk)), offset);
+            while (offset <= fileSize) {
+                await emitWithAck(socket, "chunk", {
+                    id: handshakeData.id,
+                    chunk: buf,
+                    offset: offset,
+                    fileSize: fileSize,
+                    chunkSize: Number(params.values.chunk)
+                });
+                offset = offset + Number(params.values.chunk);
+                if (offset >= fileSize) break;
+                buf = Buffer.alloc(Math.min(fileSize - offset, Number(params.values.chunk)));
+                fs.readSync(fileStream, buf, 0, Math.min(fileSize - offset, Number(params.values.chunk)), offset);
+            }
+        });
+    });
+}
+
+function emitWithAck(socket, ...args) {
+    return new Promise(function(resolve) {
+        socket.emit(...args, (...b) => resolve(...b));
+    });
+}
